6531418bca9cfbecfc452975fb22319ca82c4431
Abhay@github55 <abhayrawat2050@gmail.com>
2025-02-26 19:31:20 +0530

    latest

--- src/main/java/com/example/demo/controller/TournamentController.java
+++ src/main/java/com/example/demo/controller/TournamentController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tournaments")
-@CrossOrigin(origins = "*") 
+@CrossOrigin(origins = "https://tournament-45zp.onrender.com") 
 public class TournamentController {
 
     private final TournamentService tournamentService;
